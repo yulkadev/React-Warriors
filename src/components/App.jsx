8e61dee457fab62530c6397b410c6a6f4c02f1e7
yulkadev <yakimovayuliya@i.ua>
2021-07-07 18:27:08 +0300

    Add last changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { moviesData } from "../moviesData"
 
-function App() {
-  return <div>Hello ReactWarriors!</div>;
+class App extends React.Component{
+  constructor() {
+    super()
+
+    this.state = {
+      movies: moviesData
+    };
+  }
+
+  render() {
+    return <div>{this.state.movies.title[0]}</div>;
+  }
 }
 
 export default App;
